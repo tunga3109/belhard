5ce843a91a377b7d592a572349db862e7cca2db6
Tung <tunga3109@gmail.com>
2022-12-01 21:09:15 +0300

    add db and models

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.utils.timezone import now
+from django.contrib.auth.models import User
 
 # Create your models here.
+
 class Category(models.Model):
     name = models.CharField(
         max_length=64,
@@ -23,3 +26,41 @@ class Category(models.Model):
         verbose_name_plural = 'категории'
         db_table = 'blog_categories' # название таблицы в бд
     
+
+class Post(models.Model):
+    title = models.CharField(
+        max_length=128,
+        verbose_name='заголовок'
+    )
+    descr = models.TextField(verbose_name='описание')
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name='публикация'
+    )
+    date_created = models.DateTimeField(
+        default=now,
+        verbose_name='дата создания'
+    )
+    slug = models.SlugField(
+        verbose_name='URL',
+        unique=True
+    )
+
+    category = models.ForeignKey(
+        'Category', 
+        on_delete=models.NOT_PROVIDED, 
+        verbose_name='') # не писать название аттрибута category_id, получится category_id_id))))
+
+    author = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        verbose_name='автор')
+    
+    class Meta:
+        db_table = 'blog_posts'
+        verbose_name = 'пост'
+        verbose_name_plural = 'посты'
+        ordering = ['date_created']
+
+    def __str__(self):
+        return self.title
