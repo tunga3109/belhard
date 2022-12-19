4aecfcd26cc71095692d5dafe2c4dbf3f7758da3
Tung <tunga3109@gmail.com>
2022-12-19 20:12:18 +0300

    added Class View

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import blog_list, post_detail, contact, about
+from .views import PostListView, PostDetailView, AboutTemplateView, ContactCreateView
 
 urlpatterns = [
-    path('', blog_list, name='blog_posts'),
-    path('contact/', contact, name='blog_contact'),
-    path('about/', about, name='blog_about'),
-    path('<slug:post_slug>/', post_detail, name='blog_post')
+    path('', PostListView.as_view(), name='blog_posts'),
+    path('contact/', ContactCreateView.as_view(), name='blog_contact'),
+    path('about/', AboutTemplateView.as_view(), name='blog_about'),
+    path('<slug:post_slug>/', PostDetailView.as_view(), name='blog_post')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,89 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpRequest
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import ListView, DetailView, CreateView, TemplateView
 from django.views.decorators.http import require_GET
 
-from .models import Post
+from .models import Post, Contact
 from .forms import ContactForm
 
 
-@require_GET
-def blog_list(request: HttpRequest):
-    posts_list = Post.objects.all()
-    return render(request, "blog/index.html", {'posts': posts_list})
+class BaseMixin:
+    context = {
+        'twitter': 'https://twitter.com',
+        'facebook': 'https://facebook.com',
+        'github': 'https://github.com'
+    }
 
 
-def post_detail(request: HttpRequest, post_slug: str):
-    post = get_object_or_404(Post, slug=post_slug)
-    return render(request, 'blog/post.html', {'post': post})
+class PostListView(BaseMixin, ListView):
+    template_name = "blog/index.html"
+    context_object_name = 'posts'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(is_published=True)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
+        context['heading'] = 'MIXIN HEADING'
+        context['subheading'] = 'mixin subheading'
+        context.update(self.context)
+        return context
+
+
+class PostDetailView(BaseMixin, DetailView):
+    template_name = 'blog/post.html'
+    context_object_name = 'post'
+    slug_url_kwarg = 'post_slug'
+    model = Post
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
+        context.update(self.context)
+        return context
+
+
+class AboutTemplateView(BaseMixin, TemplateView):
+    template_name = 'blog/about.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
+        context.update(self.context)
+        context['heading'] = 'About'
+        context['coordinate'] = 'https://www.google.com/maps/d/embed?mid=1_ECVm1j8vq8hcyTlEPFP_wGe2Hk&hl=ru&ehbc=2E312F'
+        context['about'] = '''
+                aboutaboutaboutaboutaboutaboutaboutaboutabout
+                aboutaboutaboutaboutaboutaboutaboutaboutabout
+                aboutaboutaboutaboutaboutaboutaboutaboutabout
+                aboutaboutaboutaboutaboutaboutaboutaboutabout
+                aboutaboutaboutaboutaboutaboutaboutaboutabout
+        '''
+        return context
+
+
+class ContactCreateView(BaseMixin, CreateView):
+    template_name = 'blog/contact.html'
+    model = Contact
+    form_class = ContactForm
+    success_url = reverse_lazy('blog_posts')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
+        context.update(self.context)
+        context['heading'] = 'Contact'
+        return context
+
+# @require_GET
+# def blog_list(request: HttpRequest):
+#     posts_list = Post.objects.all()
+#     return render(request, "blog/index.html", {'posts': posts_list})
+
+
+# def post_detail(request: HttpRequest, post_slug: str):
+#     post = get_object_or_404(Post, slug=post_slug)
+#     return render(request, 'blog/post.html', {'post': post})
 
 
 def contact(request: HttpRequest):
@@ -26,8 +95,8 @@ def contact(request: HttpRequest):
     return render(request, 'blog/contact.html', {'contact_form': form})
 
 
-def about(request: HttpRequest):
-    return render(request, 'blog/about.html')
+# def about(request: HttpRequest):
+#     return render(request, 'blog/about.html')
 
 
 def error404(request, exception):
